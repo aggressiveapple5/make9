24496aa6308c8e3eee21d166b82d192e7e0232f9
Matthew Keller <mkeller2019@pingry.org>
2016-03-04 11:32:38 -0500

    game where user has to get 9 points. Initial Commit

---
+++ make9.py
@@ -0,0 +1,21 @@
+#Author: Matt Keller
+#Date: February 10 2016
+import random
+ready = raw_input("Type ready when you want the game to begin: ")
+ready = ready.lower()
+score = 0
+if ready == "ready":
+	print("Instructions: try to get as close to 9 points as possible without going over. \
+You will start with 0 points and be asked if you would like more points. Type 'y' to receive more points or 'n' to stop receiving them.")
+	while score < 9:
+		decide = raw_input("Your score is {}. Do you want more points? ".format(score))
+		decide = decide.lower()
+		if decide == "y":
+			score += random.uniform(0.0, 3.0)
+		elif decide == "n":
+			print("Your final score is {}. Not bad.".format(score))
+if score > 9.0:
+	print("You lose")
+	print("You score was {}.".format(score))
+if ready != "ready":
+		print("I'm sorry that you don't want to play.")
